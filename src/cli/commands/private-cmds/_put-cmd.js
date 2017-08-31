36e03336fdabd37ffeeecb21a1431e7219c8e509
GENIUS GLOBALCASH <17854935+gcbpay@users.noreply.github.com>
2017-09-01 05:14:49 +0800

    Revert "(wip) debugging the export error, this branch is temporary and will b…"

--- src/cli/commands/private-cmds/_put-cmd.js
+++ src/cli/commands/private-cmds/_put-cmd.js
@@ -1,9 +1,8 @@
 /** @flow */
 import Command from '../../command';
 import ComponentObjects from '../../../scope/component-objects';
-import { fromBase64, buildCommandMessage, packCommand, unpackCommand } from '../../../utils';
+import { fromBase64, buildCommandMessage, packCommand } from '../../../utils';
 import { put } from '../../../api/scope';
-import logger from '../../../logger/logger';
 
 export default class Put extends Command {
   name = '_put <path> <args>';
@@ -13,7 +12,6 @@ export default class Put extends Command {
   opts = [];
 
   action([path, ]: [string, string]): Promise<any> {
-    logger.debug('_put in action');
     let data = '';
     return new Promise((resolve, reject) => {
       process.stdin
@@ -28,12 +26,6 @@ export default class Put extends Command {
   }
 
   report(componentObjects: ComponentObjects): string {
-    logger.debug('_put in report');
-    const compsStr = ComponentObjects.manyToString(componentObjects);
-    const compsStrPacked = packCommand(buildCommandMessage(compsStr));
-    const compsStrUnpacked = unpackCommand(compsStrPacked); // observe that it is able to parse the json at this step
-    // logger.debug(`_put before sending ${compsStrPacked}`);
-    logger.debug(`_put before sending length ${compsStrPacked.length}`);
     return packCommand(buildCommandMessage(ComponentObjects.manyToString(componentObjects)));
   }
 }
